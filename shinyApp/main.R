bb4b441c98f7e9cb3d5fdea124e736f31bcb3712
earnaud <43677175+earnaud@users.noreply.github.com>
2019-04-10 17:22:19 +0200

    Add files via upload

--- shinyApp/main.R
+++ shinyApp/main.R
@@ -13,11 +13,24 @@ IM.navTree = c("navTreeModule", "navTree", "navTreeSelect")
 
 ### UI ###
 ui <- fluidPage(
+  # Styling and other user-friendly settings
+  tags$head(
+    tags$style(
+      HTML("
+           pre {
+              white-space: pre-wrap;
+              word-break: keep-all;
+           }
+           ")
+    )
+  ),
+  
     navTreeUI(IM.navTree[1], IM = IM.navTree)
 )
 
 ### SERVER ###
 server <- function(input,output,session){
+  session$onSessionEnded(stopApp)
   
   # modules called
   output <- callModule(navTree, IM.navTree[1], IM = IM.navTree)
