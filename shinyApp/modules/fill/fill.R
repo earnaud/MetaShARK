2432098a84ac2d76e2298ef3f67e3da994953f5f
earnaud <43677175+earnaud@users.noreply.github.com>
2019-05-10 18:10:01 +0200

    Add files via upload

--- shinyApp/modules/fill/fill.R
+++ shinyApp/modules/fill/fill.R
@@ -1,16 +1,33 @@
 # documentation.R
 
 ### IMPORTS ###
+library("data.table")
 
-
+cat("* Loading Fill Guidelines: \n")
+fillGuideline = as.list(readRDS("resources/fillGuideline.RData"))
+cat("** Fill Guideline successfully loaded !\n")
+minFillGuideline = as.data.frame(read.table("resources/minFillGuideline.tsv" ))
+cat("** Minimized Fill Guideline successfully loaded !\n")
 
 ### UI ###
-fill.UI <- function(id, IM){
+fillUI <- function(id, IM){
   ns <- NS(id)
   
-  pageWithSidebar(
-    sidebarPanel("Terms"),
-    mainPanel("Input form")
+  div(
+    fluidRow(
+      box(column(4,
+                 selectInput(ns("item"),
+                             "Select an item to fill:",
+                             minFillGuideline$abrev,
+                             selectize = FALSE)),
+          column(8,
+                 htmlOutput(ns("location")),
+                 style="max-height: 10%;"),
+          width = 12)
+    ),
+    fluidRow(
+      uiOutput(ns("accessed"))
+    )
   )
 }
 
@@ -18,5 +35,26 @@ fill.UI <- function(id, IM){
 
 ### SERVER ###
 fill <- function(input, output, session, IM){
+  # This commented part is useful to get str() applied on a list
+  # output$accessedPath <- reactive({gsub("((\\.\\. *)*\\$ )","\n\\1",
+  #                                       gsub(" +"," ",
+  #                                            capture.output(str(followPath(systemGuideline,input$item)))))})
+
+  output$location <- renderText({ 
+    row <- which(minFillGuideline[,2] == input$item)
+    return(HTML(paste0("<b>SystemGuideline Location: </b>", as.character(minFillGuideline[row,1]))))
+  })
+  
+  output$accessed <- renderUI({
+    row <- which(minFillGuideline[,2] == input$item)
+    accessed <- followPath(systemGuideline, as.character(minFillGuideline[row,1]))  
+    ac.names <- gsub("[0-9]+_","", attr(accessed, "names"))
+    lapply(ac.names,
+           function(name)
+             box("Still nothin",
+                 title = name,
+                 width = 4)
+           )
+  })
   
 }
